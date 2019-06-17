50011aa15cf7396bd03d8b872677b87c8703f0fd
Brian Phelps <phelpsb92@gmail.com>
2019-06-16 23:25:19 -0400

    landing page finish

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,10 @@ router.get('/', (req, res, next) =>  {
   res.render('index.ejs', { title: 'Express', user: req.user });
 });
 
+router.post('/submit-phone/', function(req, res){
+  var phone = req.body.phone;
+  
+})
+
 
 module.exports = router;

e8485333d80243bfd56a3cd6ce515bc4c5b78284
Brian Phelps <phelpsb92@gmail.com>
2019-07-02 18:23:13 -0400

    user numpad functional

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,39 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var User = require('../models/user');
+var internetAvailable = require("internet-available");
 
 /* GET home page. */
-router.get('/', (req, res, next) =>  {
+router.get('/', hasInternet, (req, res, next) =>  {
   res.render('index.ejs', { title: 'Express', user: req.user });
 });
 
-router.get('/enter_mobile', function(req, res){
+router.get('/enter_mobile', hasInternet, function(req, res){
   res.render('enter_mobile.ejs', {
     title: 'Enter Mobile',
     user: req.user
   })
 })
 
-router.post('/submit-phone/', function(req, res){
+router.post('/submit-phone/', hasInternet, function(req, res){
   var phone = req.body.phone;
 
 })
 
 
 module.exports = router;
+
+function hasInternet(req, res, next){
+  internetAvailable().then(function(){
+    console.log("Internet available");
+    next();
+  }).catch(function(){
+    //TODO
+    //display a 'CONNECTION ERROR'
+    console.log("Houston we have a problem")
+    res.render("require_internet.ejs", {
+      title: 'Connection Error',
+      user: req.user
+    })
+  });
+}
